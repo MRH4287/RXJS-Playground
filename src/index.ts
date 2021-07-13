3f388ba3134b6a7ad7c2e3a7dce15168db93728b
Manuel Holzhauser <Manuel@mrh-development.de>
2021-07-13 10:34:05 +0200

    Update Playground

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
-import { forkJoin, merge, Observable, of, Subject, timer } from "rxjs";
+import { BehaviorSubject, forkJoin, merge, Observable, of, Subject, timer } from "rxjs";
 import { delay, distinctUntilChanged, filter, finalize, map, mergeMap, startWith, switchMap, take, takeUntil, tap } from 'rxjs/operators';
 import { Api, ApiResult } from "./fakeApi";
+import { Playground } from "./playground";
 
-// -- Setup
-const setResult = (value: any) => {
-  document.getElementById('result').innerText = String(value);
-  console.log(value);
-}
+/*
+  What is on the Playground?
+  - A button
+  - An Output-Element
+  - A list of indicators
 
-const buttonClick = new Subject<MouseEvent>();
-const button: HTMLButtonElement = <HTMLButtonElement>document.getElementById('button');
-button.onclick = event => buttonClick.next(event);
 
-// ---------- End Setup
+  Explaination for Playground-Class:
+  - It allows the interaction with the Playground
+  - It automates small, not important, tasks which are used for visualization
+
+
+  What methods are used?
+  - getButton -> Returns the instance of the Button used in the Playground
+  - getButtonEvent -> Returns an Observable that is triggered when the button is used
+  - setResult -> Prints a result to the Result-Element and console
+  - lightIcon -> Generates a pipeable method that briefly flash one of the indicators
+  - lightIconImediate -> briefly flash one of the indicators
+  - enableIndicator -> Enable a given count of indicators for the current test
+  - makePublic -> Make sure some RxJs Classes are in the Window, to test some data
+
+*/
+
+Playground.makePublic();
+
+// Hide Icons for now
+Playground.enableIcon(0);
 
 
 /*
@@ -37,29 +54,219 @@ button.onclick = event => buttonClick.next(event);
 
 */
 
-buttonClick.pipe(
-  tap(() => button.disabled = true),
-  mergeMap(event => Api.getResultRandom(event)),
-  map(e => JSON.stringify({
-    success: e.success,
-    data: {
-      x: e.data?.clientX,
-      y: e.data?.clientY
-    }
-  })),
-  tap(() => button.disabled = false)
-).subscribe(result => {
-  setResult(result);
-});
 
-const observables: Observable<ApiResult<number>>[] = [];
-for (let i = 0; i < 100; i++) {
-  observables[i] = Api.getResultRandom(i);
+/* Part1 -- How does RxJS work? */
+// Try in Console:
+// var event = Playground.getButtonEvent();
+// try pressing the button
+
+//var subscription = event.subscribe(data => console.log(data));
+// try pressing the button
+
+// subscription.unsubscribe();
+// try pressing the button
+
+
+
+
+
+
+
+
+
+
+
+/* Part2 -- How does RxJS work? */
+
+if (false) {
+
+  Playground.enableIcon(1);
+
+  const dataSource = new Subject<string>();
+  dataSource
+    .pipe(
+      Playground.lightIcon(1),
+      tap(result => Playground.setResult(result))
+    ).subscribe();
+    // .subscribe(result => {
+
+    //   Playground.setResult(result);
+
+    // });
+
+  // What happens?
+  //A: Nothing. Why? -> No Data
+
+  dataSource.next("Hi");
+
+
+  // -------------- Closing of Observable -------
+  dataSource.complete();
+  dataSource.next("Hello World");
+
+  // Try in console:
+
+  // var data = new Subject();
+  // data.subscribe(el => console.log(el));
+  // data.next("Data");
+  // data.complete();
+  // data.next("Data 2");
+
+
+
 }
+
+
+/* Part 3 - Data Sources */
+
+if (false) {
+
+  const subject = new Subject<string>();
+  // Replace with: new BehaviorSubject<string>(null);
+
+  subject.next("1");
+  subject.subscribe(value => Playground.setResult(value));
+
+  subject.next("2");
+  subject.next("3");
+
+
+  // Static Source
+
+  const ofSource = of('Hello World');
+  const ofSourceSubscription = ofSource.subscribe(v => Playground.setResult(v));
+
+  Playground.setResult('Is Completed? ' + ofSourceSubscription.closed)
+
+
+}
+
+
+
+/* Part 4 -- Transformations */
+
+if (false) {
+
+
+  of('hello world').pipe(
+
+    startWith('start'),
+    tap(input => Playground.setResult(input), error => console.error(error), () => console.log('Completed')),
+    map(input => {
+      return input.toUpperCase();
+    })
+
+  ).subscribe(Playground.setResult);
+
+}
+
+
+
+
+/* Part 5 -- Filter */
+
+if (false) {
+
+  // Delay 0ms - Every 1000ms
+
+  const source = timer(0, 500);
+
+
+  source
+    .pipe(
+      filter(input => input % 2 === 0)
+    )
+    .subscribe(Playground.setResult);
+
+
+}
+
+
+
+
+/* Part 6 -- Async Methods */
+
+if (true) {
+  Playground.enableIcon(3);
+
+
+  // Api.echoCall("Hello World").subscribe(Playground.setResult);
+
+
+  // Anti-Pattern
+  // Playground.getButtonEvent().subscribe(() => {
+  //   Playground.lightIconImediate(1);
+
+  //   Api.echoCall("Call 1").subscribe(result => {
+  //     Playground.lightIconImediate(2);
+  //     Playground.setResult(result);
+  //     if (result.success) {
+
+  //       Api.echoCall("Call 2").pipe(Playground.lightIcon(3)).subscribe(Playground.setResult);
+  //     } else {
+  //       Api.echoCall("Call 3").pipe(Playground.lightIcon(3)).subscribe(Playground.setResult);
+  //     }
+  //   })
+  // });
+
+
+  Playground.getButtonEvent().pipe(
+    Playground.lightIcon(1),
+    mergeMap(() => {
+      return Api.echoCall("Call 1").pipe(
+        tap(Playground.setResult),
+        Playground.lightIcon(2),
+        mergeMap(result => {
+          if (result.success) {
+            return Api.echoCall("Call 2");
+          } else {
+            return Api.echoCall("Call 3");
+          }
+        })
+      )
+    }),
+    Playground.lightIcon(3)
+  ).subscribe(Playground.setResult);
+
+
+
+}
+
+
+// const buttonClick = Playground.getButtonEvent();
+// const button = Playground.getButton();
+
+// buttonClick.pipe(
+//   tap(() => button.disabled = true),
+//   Playground.lightIcon('icon1'),
+//   mergeMap(event => Api.getResultRandom(event)),
+//   map(e => JSON.stringify({
+//     success: e.success,
+//     data: {
+//       x: e.data?.clientX,
+//       y: e.data?.clientY
+//     }
+//   })),
+//   tap(() => button.disabled = false),
+//   Playground.lightIcon('icon2'),
+// ).subscribe(result => {
+//   Playground.setResult(result);
+// });
+
+// const observables: Observable<ApiResult<number>>[] = [];
+// for (let i = 0; i < 100; i++) {
+//   observables[i] = Api.getResultRandom(i);
+// }
 
 // forkJoin(observables).subscribe(res => setResult(res));
 // merge(...observables).subscribe(res => setResult(res));
 
+
+// of(2).pipe(
+//   switchMap(val => {
+//     return timer(1, 23).pipe(map(_ => val))
+//   })
+// );
 
 
 
---
+++ src/playground.ts
@@ -0,0 +1,117 @@
+import { BehaviorSubject, fromEvent, Observable, of, Subject, timer } from "rxjs";
+import { map, mergeMap, switchMap, tap } from "rxjs/operators";
+import { Api } from "./fakeApi";
+
+/**
+ * Used to control the PlayGround Elements
+ */
+export class Playground {
+
+  public static getButton(): HTMLButtonElement {
+    return <HTMLButtonElement>document.getElementById('button');
+  }
+
+  private static _buttonEvent: Observable<MouseEvent>;
+
+  public static getButtonEvent(): Observable<MouseEvent> {
+    if (!!this._buttonEvent) {
+      return this._buttonEvent;
+    }
+
+
+    const button = this.getButton();
+    if (!button) {
+      return of(null);
+    }
+
+    this._buttonEvent = <Observable<MouseEvent>>fromEvent(button, 'click');
+    return this._buttonEvent;
+  }
+
+  public static setResult(value: any, append = true) {
+    const element = document.getElementById('result');
+    const input = typeof(value) === 'object' ? JSON.stringify(value) : String(value);
+    const lines = element.innerText.split('\n').length;
+    if (lines > 8) {
+      element.innerText = '';
+    }
+
+    if (!append) {
+      element.innerText = input;
+    } else {
+      element.innerText += input + '\n';
+    }
+
+    console.log(value);
+  }
+
+
+  public static lightIcon<T>(icon: string | number): (input: Observable<T>) => Observable<T> {
+    return (input) => {
+      return input.pipe(
+        tap(_ => {
+          Playground.lightIconImediate(icon);
+        })
+      );
+    };
+  }
+
+  public static lightIconImediate(icon: string | number) {
+    if (typeof(icon) === 'number') {
+      icon = `icon${icon}`;
+    }
+
+    this.setIcon(<string>icon, true);
+
+    setTimeout(() => {
+      this.setIcon(<string>icon, false);
+    }, 200);
+  }
+
+  private static setIcon(icon: string, state: boolean) {
+    const iconElement = <HTMLDivElement>document.getElementById(icon);
+    this.setClass(iconElement, 'trigger', state);
+  }
+
+  private static setClass(element: HTMLDivElement, cssClass: string, state: boolean) {
+    if (!element) {
+      return;
+    }
+    const isSet = element.classList.contains(cssClass);
+    if (state && !isSet){
+      element.classList.add(cssClass);
+    } else if (!state && !!isSet) {
+      element.classList.remove(cssClass);
+    }
+  }
+
+  public static enableIcon(count: number) {
+    const icons = document.querySelectorAll(".icon");
+    icons.forEach((icon: HTMLDivElement) => this.setClass(icon, 'hidden', true));
+
+    for (let i = 1; i <= count; i++) {
+      const icon = <HTMLDivElement>document.getElementById('icon' + i);
+      this.setClass(icon, 'hidden', false);
+    }
+  }
+
+  public static makePublic() {
+    const win = <any>window;
+
+    win.Playground = Playground;
+    win.Api = Api;
+
+    win.Subject = Subject;
+    win.Observable = Observable;
+    win.BehaviorSubject = BehaviorSubject;
+    win.tap = tap;
+    win.map = map;
+    win.of = of;
+    win.timer = timer;
+    win.mergeMap = mergeMap;
+    win.switchMap = switchMap;
+
+  }
+
+
+}
